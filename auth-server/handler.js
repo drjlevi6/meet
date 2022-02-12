8440be96b21fd0214cbcda7ff3c3ca60a91bd2f1
drjlevi6 <drjlevi6@aol.com>
2022-02-12 16:28:30 -0500

    Update handler.js
    Added headers to .catch handler

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -144,6 +144,10 @@ module.exports.getCalendarEvents = async (event) => {
     console.error(err);
     return {
       statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true
+      },
       body: JSON.stringify(err)
     };
   });
