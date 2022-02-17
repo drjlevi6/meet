ac26d8774a354deeca2bb9f2f9b8ae2f5b4a8403
drjlevi6 <drjlevi6@aol.com>
2022-02-17 17:33:44 -0500

    Delete logo.svg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -9,20 +8,6 @@ function App() {
    */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! --(Line modified by jl)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
