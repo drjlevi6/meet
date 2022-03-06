770bd09da44510344eb1050da1df21ba8dfb1912
drjlevi6 <drjlevi6@aol.com>
2022-03-06 14:30:19 -0500

    Update Event.js, Event.test.js
    Event.js: Inserted console.log statements, showing that setState command on isExpanded is working
    Event.test.js: Defined EventWrapper with a beforeAll call; tried a different way to access isExpanded with "const {isExpanded} = this.state;" which failed.

--- src/Event.js
+++ src/Event.js
@@ -2,19 +2,26 @@ import React, { Component } from "react";
 
 class Event extends Component {
   state = { isExpanded: false, details: [] }
-  render() { // was  this.props.event.summary
+  render() { // returned <h2> was this.props.event.summary
      return <div>
       <h2>{ this.props.summary }</h2>
       {
         this.state.isExpanded &&
-        <div className='event-details'><p>{ this.props.event.start.dateTime }</p></div> 
+        <div className='event-details'>
+          <p>{ this.props.event.start.dateTime }</p>
+        </div> 
       }
-      <button onClick={() => this.toggleExpandedDetails()}>Show/Hide Details</button>
+      <button onClick={() => 
+        this.toggleExpandedDetails()}>Show/Hide Details</button>
     </div>;
   }
   toggleExpandedDetails() {
-    console.log('toggleExpandedDetails')
+    console.log('Event.js, toggleExpandedDetails, ' + 
+      'isExpanded before setState:', this.state.isExpanded);
     this.setState({isExpanded: !this.state.isExpanded});
+    console.log('Event.js, toggleExpandedDetails, ' + 
+    'isExpanded after setState:', this.state.isExpanded);
+
   }
 }
 export default Event;
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -4,22 +4,22 @@ import Event from '../Event';
 import { mockData } from '../mock-data';
 
 describe('<Event /> component', () => {
+  let EventWrapper;
+  beforeAll(() => {
+      EventWrapper = shallow(<Event event={mockData[0]} />);
+  })
+
   test("show an event's details", () => {
-    const EventWrapper = shallow(
-      <Event event={mockData[0]}/>
-    );
-      expect(EventWrapper.find(".event-details").exists()).toEqual(false);
-      expect(EventWrapper.find('button').exists()  ).toEqual(true);
-      EventWrapper.find('button').simulate('click');  /* not sure we need '.first()'
-                                            after EventWrapper.find('button') */
-      expect(EventWrapper.find(".event-details").exists()).toEqual(true); // appears to work
+    const {isExpanded} = this.state;
+    expect(EventWrapper.find(".event-details").exists()).toEqual(false);
+    expect(EventWrapper.find('button').exists()  ).toEqual(true);
+
+    //  not sure we needed '.first()'  after EventWrapper.find('button')
+    EventWrapper.find('button').simulate('click'); 
+    expect(EventWrapper.find(".event-details").exists()).toEqual(true); // appears to work
   });
 
   test("hide an event's details", () => {
-    const EventWrapper = shallow(
-      <Event event={mockData[0]}/>
-    );
     console.log('isExpanded:', EventWrapper.isExpanded);
   });
-  
 });
