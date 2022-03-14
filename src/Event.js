e9d868d63b7ec86ab36fa8670c21b0678c04d869
drjlevi6 <drjlevi6@aol.com>
2022-03-14 15:15:30 -0400

    Update Event.js
    Corrected syntax errors

--- src/Event.js
+++ src/Event.js
@@ -2,19 +2,22 @@ import React, { Component } from "react";
 
 class Event extends Component {
   state = { isExpanded: false, details: [] }
+  eData = this.props.event;
   render() {
-    return <div  className='event-div'>
-      <h2 className='summary'>{this.props.event.summary}</h2>
-     
-      {
-        this.state.isExpanded &&
-        <div className='event-details'>
-          <p>{ this.props.event.start.dateTime }</p>
-        </div> 
-      }
-      <button onClick={() => 
-        this.toggleExpandedDetails()}>Show/Hide Details</button>
-    </div>;
+    return (
+      <div  className='event-div'>
+        <h2 className='summary'>{eData.summary}</h2>   
+        {
+          this.state.isExpanded &&
+          <div className='event-details'>
+            <p>{ eData.start.dateTime }
+              ({eData.start.timeZone} Time)</p>
+          </div> 
+        }
+        <button onClick={() => 
+          this.toggleExpandedDetails()}>Show/Hide Details</button>
+      </div>
+    )
   }
 
   toggleExpandedDetails() {
